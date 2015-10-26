8d2127ca9814ba8d57513bee1b3d29047e1e2ef8
Evan Knox <eknoxmobile@gmail.com>
2015-10-26 12:27:02 -0400

    Commit of notes and Odometer

--- DPM_FINAL/src/robot/Robot.java
+++ DPM_FINAL/src/robot/Robot.java
@@ -2,9 +2,15 @@ package robot;
 
 public interface Robot {
 	public void moveTo();
+	
 	public void turnTo(int angle);
 	
 	public int usScan();
+	
 	public int colorScan();
+	
 	public void capture();
+	
+	public void localize();
+	
 }
